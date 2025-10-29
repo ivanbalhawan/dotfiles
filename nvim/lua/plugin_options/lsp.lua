17ef07ad47d2066b1be0287e6343c23b3308c0d1
ivan <ivanbalhawan@proton.me>
2025-10-29 16:52:25 +0100

    lsp config changes in nvim

--- nvim/lua/plugin_options/lsp.lua
+++ nvim/lua/plugin_options/lsp.lua
@@ -1,51 +1,14 @@
+-- Mason setup
 require("mason").setup()
-require("mason-lspconfig").setup()
-local telescope_builtin = require("telescope.builtin")
-
-local cmp_capabilities = require("cmp_nvim_lsp").default_capabilities()
-
-vim.lsp.config('pylsp', {
-    capabilities = cmp_capabilities,
-    settings = {
-		pylsp = {
-			plugins = {
-				pycodestyle = {
-					maxLineLength = 100,
-				},
-			},
-		},
-    },
+require("mason-lspconfig").setup({
+	ensure_installed = { "ruff", "pylsp" },
 })
 
--- lsp.lua_ls.setup({
--- 	capabilities = cmp_capabilities,
--- })
-
--- lsp.markdown_oxide.setup({})
-
--- lsp.pylsp.setup({
--- 	-- single_file_support = false,
--- 	capabilities = cmp_capabilities,
--- 	settings = {
--- 		pylsp = {
--- 			plugins = {
--- 				pycodestyle = {
--- 					maxLineLength = 100,
--- 				},
--- 			},
--- 		},
--- 	},
--- })
-
+-- Formatter setup with conform.nvim
 require("conform").setup({
-	-- formatters = {
-	-- 	black = {
-	-- 		prepend_args = { "--fast" },
-	-- 	},
-	-- },
 	formatters_by_ft = {
 		lua = { "stylua" },
-		python = { "isort", "ruff_format" },
+		python = { "isort", "ruff_format" }, -- isort first, then Ruff for formatting
 		bash = { "shfmt" },
 		shell = { "shfmt" },
 		sh = { "shfmt" },
@@ -54,30 +17,84 @@ require("conform").setup({
 		markdown = { "mdformat" },
 	},
 })
+
 vim.keymap.set("n", "<leader>fo", function()
 	require("conform").format({ async = true })
-end)
+end, { desc = "Format file" })
 
--- Use LspAttach autocommand to only map the following keys
--- after the language server attaches to the current buffer
+local telescope_builtin = require("telescope.builtin")
+local cmp_capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+vim.lsp.config("pylsp", {
+	capabilities = cmp_capabilities,
+	settings = {
+		pylsp = {
+			plugins = {
+				pycodestyle = {
+					maxLineLength = 100,
+				},
+			},
+		},
+	},
+})
+-- vim.lsp.config("pyright", {
+-- 	settings = {
+-- 		pyright = {
+-- 			disableOrganizeImports = true,
+-- 		},
+-- 		python = {
+-- 			analysis = {
+-- 				-- Ignore all files for analysis to exclusively use Ruff for linting
+-- 				ignore = { "*" },
+-- 			},
+-- 		},
+-- 	},
+-- })
+-- vim.lsp.enable("pyright")
+vim.lsp.enable("pylsp")
+vim.lsp.config("ruff", {
+	settings = {
+		configurationPreference = "filesystemFirst",
+		lint = {
+			enable = true,
+			preview = true,
+		},
+	},
+})
+vim.lsp.enable("ruff")
+
+-- LspAttach: runs after LSP attaches to buffer
 vim.api.nvim_create_autocmd("LspAttach", {
 	group = vim.api.nvim_create_augroup("UserLspConfig", {}),
 	callback = function(ev)
-		-- Enable completion triggered by <c-x><c-o>
+		-- Enable omnifunc for completion
 		vim.bo[ev.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
 
-		-- Buffer local mappings.
-		-- See `:help vim.lsp.*` for documentation on any of the below functions
+		-- Keymaps for LSP actions
 		local opts = { buffer = ev.buf }
 		vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
-		vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-		vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-		vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float)
-		vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist)
+		vim.keymap.set("n", "gd", telescope_builtin.lsp_definitions, opts)
+		vim.keymap.set("n", "gr", telescope_builtin.lsp_references, opts)
 		vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+		vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
 		vim.keymap.set("n", "<leader>D", vim.lsp.buf.type_definition, opts)
 		vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
 		vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts)
-		vim.keymap.set("n", "gr", telescope_builtin.lsp_references, {})
+		vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, opts)
+		vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, opts)
 	end,
+})
+vim.api.nvim_create_autocmd("LspAttach", {
+	group = vim.api.nvim_create_augroup("lsp_attach_disable_ruff_hover", { clear = true }),
+	callback = function(args)
+		local client = vim.lsp.get_client_by_id(args.data.client_id)
+		if client == nil then
+			return
+		end
+		if client.name == "ruff" then
+			-- Disable hover in favor of Pyright
+			client.server_capabilities.hoverProvider = false
+		end
+	end,
+	desc = "LSP: Disable hover capability from Ruff",
 })
--- nvim/lua/plugin_options/telescope.lua
+++ nvim/lua/plugin_options/telescope.lua
@@ -33,6 +33,8 @@ require("telescope").setup({
 vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
 vim.keymap.set("n", "<leader>fg", builtin.git_files, {})
 vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
+vim.keymap.set("n", "<leader>fj", builtin.jumplist, {})
+vim.keymap.set("n", "<leader>fq", builtin.quickfixhistory, {})
 vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
 vim.keymap.set("n", "<leader>fs", builtin.live_grep, {})
 vim.keymap.set("n", "<leader>fp", builtin.registers, {})

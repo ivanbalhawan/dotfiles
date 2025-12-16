bab642cae3c235450657a55301d61728550469d4
ivan <ivanbalhawan@proton.me>
2025-12-16 10:48:23 +0100

    remove dotfiles for applications i no longer use

--- sway/scripts/add-mark.sh
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/bash
-new_mark=[\"$1\"]
-criteria=$2
-existing_marks=$(swaymsg -t get_marks | jq)
-
-new_mark_exists=$(echo $new_mark | jq --argjson marks "$existing_marks" 'inside($marks)')
-if [[ $new_mark_exists == "false" ]]; then
-    swaymsg "$criteria mark --add $1"
-fi
--- waybar/launch-waybar.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-WAYBAR_DIR=$HOME/.config/waybar
-eza $WAYBAR_DIR/config $WAYBAR_DIR/style.css | entr -rn waybar

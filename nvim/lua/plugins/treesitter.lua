00c1120541d2b60f4444909417080eab4125bc1c
ivan <ivanbalhawan@proton.me>
2026-04-21 13:23:44 +0200

    fix treesitter nvim config

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -3,15 +3,17 @@ return {
         "nvim-treesitter/nvim-treesitter",
         build = ":TSUpdate",
         branch = "master",
-        opts = {
-            ensure_installed = { "c", "lua", "python", "vim", "vimdoc", "query" },
-            sync_install = false,
-            auto_install = true,
-            highlight = {
-                enable = true,
-                additional_vim_regex_highlighting = false,
-            },
-        },
+        config = function()
+            require("nvim-treesitter.configs").setup({
+                ensure_installed = { "c", "lua", "python", "vim", "vimdoc", "query" },
+                sync_install = false,
+                auto_install = true,
+                highlight = {
+                    enable = true,
+                    additional_vim_regex_highlighting = false,
+                },
+            })
+        end,
     },
     {
         "nvim-treesitter/nvim-treesitter-context",
